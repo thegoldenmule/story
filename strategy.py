273aac6226a6e70524b2761f0b4ac5b36a961bd3
Benjamin Jordan <bmj465@gmail.com>
2014-03-09 14:50:21 -0700

    Added an initial chunking strategy

--- strategy.py
+++ strategy.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.tag.simplify import simplify_wsj_tag
+from nltk.corpus import conll2000
 
 
 class Parser:
@@ -23,9 +24,80 @@ class Parser:
         return sentence_data, word_data
 
 
-class NounResolutionStrategy:
+class EntityResolutionStrategy:
     def __init__(self):
         pass
 
     def resolve(self, taggedWords):
         return (word for (word, pos) in taggedWords if pos == 'NN')
+
+
+class ConsecutiveNPChunkTagger(nltk.TaggerI):
+
+    def __init__(self, train_sentences):
+        train_set = []
+        for tagged_sent in train_sentences:
+            untagged_sent = nltk.tag.untag(tagged_sent)
+            history = []
+            for i, (word, tag) in enumerate(tagged_sent):
+                featureset = self.features(untagged_sent, i, history)
+                train_set.append( (featureset, tag) )
+                history.append(tag)
+        self.classifier = nltk.MaxentClassifier.train(
+            train_set,
+            algorithm='megam',
+            trace=0)
+
+    def tag(self, sentence):
+        history = []
+        for i, word in enumerate(sentence):
+            featureset = self.features(sentence, i, history)
+            tag = self.classifier.classify(featureset)
+            history.append(tag)
+        return zip(sentence, history)
+
+    def features(self, sentence, i, history):
+        word, pos = sentence[i]
+        if i == 0:
+            prevword, prevpos = "<START>", "<START>"
+        else:
+            prevword, prevpos = sentence[i-1]
+
+        if i == len(sentence)-1:
+            nextword, nextpos = "<END>", "<END>"
+        else:
+            nextword, nextpos = sentence[i+1]
+
+        return {"pos": pos,
+            "word": word,
+            "prevpos": prevpos,
+            "nextpos": nextpos,
+            "prevpos+pos": "%s+%s" % (prevpos, pos),
+            "pos+nextpos": "%s+%s" % (pos, nextpos),
+            "tags-since-dt": self.tags_since_dt(sentence, i)}
+
+    def tags_since_dt(self, sentence, i):
+        tags = set()
+
+        for word, pos in sentence[:i]:
+            if pos == 'DT':
+                tags = set()
+            else:
+                tags.add(pos)
+
+        return '+'.join(sorted(tags))
+
+
+class ConsecutiveNPChunker(nltk.ChunkParserI):
+
+    def __init__(self, train_sentences):
+        tagged_sentences = [
+            [((word, tag), chunk) for (word, tag, chunk) in
+                nltk.chunk.tree2conlltags(sent)]
+            for sent in train_sentences]
+        self.tagger = ConsecutiveNPChunkTagger(tagged_sentences)
+
+    def parse(self, sentence):
+        tagged_sentences = self.tagger.tag(sentence)
+        conll_tags = [(word, tag, chunk) for ((word, tag), chunk) in tagged_sentences]
+        return nltk.chunk.conlltags2tree(conll_tags)
